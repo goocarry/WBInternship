7b07bd4328b30678914751f3046b3dace319b501
goocarry <goocarry@gmail.com>
2022-09-26 16:39:25 +0900

    basic cache

---
+++ internal/cache/cache.go
@@ -0,0 +1,53 @@
+package cache
+
+import (
+	"sync"
+	"time"
+
+	"github.com/goocarry/wb-internship/internal/model"
+)
+
+// Cache ...
+type Cache struct {
+	sync.RWMutex
+	orders map[string]model.Order
+}
+
+// New ...
+func New(defaultExpiration, cleanupInterval time.Duration) *Cache {
+
+	orders := make(map[string]model.Order)
+
+	cache := Cache{
+		orders: orders,
+	}
+
+	return &cache
+}
+
+// Set ...
+func (c *Cache) Set(key string, value interface{}) {
+
+	c.Lock()
+
+	defer c.Unlock()
+
+	c.orders[key] = model.Order{}
+}
+
+// Get ...
+func (c *Cache) Get(key string) (interface{}, bool) {
+
+	c.RLock()
+
+	defer c.RUnlock()
+
+	order, found := c.orders[key]
+
+	// key not found
+	if !found {
+		return nil, false
+	}
+
+	return order, true
+}
